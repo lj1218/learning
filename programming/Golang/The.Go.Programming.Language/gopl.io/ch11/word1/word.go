b213be0e45a32886c2d997ac7cfe39fd33eaa86b
lj1218 <lj_ebox@163.com>
2019-06-16 23:47:10 +0800

    GoPL: add codes of learning ch11

---
+++ programming/Golang/The.Go.Programming.Language/gopl.io/ch11/word1/word.go
@@ -0,0 +1,11 @@
+// Package word provides utilities for word games.
+package word
+
+func IsPalindrome(s string) bool {
+	for i := range s {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
