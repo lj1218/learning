c1c4372813e440078ea8feeca2c7fd3a96335627
lj1218 <lj_ebox@163.com>
2019-06-16 15:22:50 +0800

    GoPL: add codes of learning ch10

---
+++ programming/Golang/The.Go.Programming.Language/gopl.io/ch10/jpeg/jpeg.go
@@ -0,0 +1,30 @@
+// The jpeg command reads a PNG image from the standard input
+// and writes it as a JPEG image to the standard output.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/png"
+	"io"
+	"os"
+)
+
+func main() {
+	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func toJPEG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+}
+
+// go run jpeg.go <1.png >1.jpeg
