5ecc404f21320e1ce07520eca9f818a451114a7f
lj1218 <lj_ebox@163.com>
2019-06-17 18:02:50 +0800

    GoPL: add codes of learning ch11 - Optimize word with benchmark

--- programming/Golang/The.Go.Programming.Language/gopl.io/ch11/word2/word.go
+++ programming/Golang/The.Go.Programming.Language/gopl.io/ch11/word2/word.go
@@ -8,16 +8,26 @@ import (
 // IsPalindrome reports whether s reads the same forward and backward.
 // Letter case is ignored, as are non-letters.
 func IsPalindrome(s string) bool {
-	var letters []rune
+	//var letters []rune
+	var letters = make([]rune, 0, len(s))
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
 		}
 	}
-	for i := range letters {
+
+	//for i := range letters {
+	//	if letters[i] != letters[len(letters)-1-i] {
+	//		return false
+	//	}
+	//}
+	n := len(letters) / 2
+	for i := 0; i < n; i++ {
 		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
 	}
 	return true
 }
+
+// go test -bench=. -benchmem
