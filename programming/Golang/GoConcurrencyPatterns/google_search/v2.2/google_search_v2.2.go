90c887ad70bc5f995484a62e855d3cb57bc08a40
lj1218 <lj_ebox@163.com>
2019-06-23 14:01:32 +0800

    GoConcurrencyPatterns: add google search

---
+++ programming/Golang/GoConcurrencyPatterns/google_search/v2.2/google_search_v2.2.go
@@ -0,0 +1,43 @@
+// Run the Web, Image, and Video searches concurrently, and wait for all results.
+// No locks. No condition variables. No callbacks.
+// Avoid timeout
+// Q: How do we avoid discarding results from slow servers?
+// A: Replicate the servers. Send requests to multiple replicas, and use the first response.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Result string
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+	}
+}
+
+func First(query string, replicas ...Search) Result {
+	c := make(chan Result)
+	searchReplica := func(i int) { c <- replicas[i](query) }
+	for i := range replicas {
+		go searchReplica(i)
+	}
+	return <-c
+}
+
+// Using the First function
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	result := First("golang",
+		fakeSearch("replica 1"),
+		fakeSearch("replica 2"))
+	elapsed := time.Since(start)
+	fmt.Println(result)
+	fmt.Println(elapsed)
+}
